5c3a77f70699c8cf11a6b07a4527dd55d7fe8a6b
younghyun.j <zerohyun.j@gmail.com>
2024-10-27 23:08:58 +0900

    streaming-settlement project init

---
+++ src/test/java/com/streaming/StreamingSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingSettlementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
