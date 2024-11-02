03a4e0a31204fb5ae819decfc0e608cb09102ff6
younghyun.j <zerohyun.j@gmail.com>
2024-11-02 15:01:11 +0900

    [feat] batch-server project init #2
    * batch, redis, queryDSL등 의존성 추가 및 queryDSL 설정

---
+++ batch-server/src/main/java/com/streaming/settlement/batchserver/BatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.batchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServerApplication.class, args);
+	}
+
+}
---
+++ batch-server/src/test/java/com/streaming/settlement/batchserver/BatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.settlement.batchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
